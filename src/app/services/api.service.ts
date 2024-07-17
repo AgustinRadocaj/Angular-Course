2e4d46a9bf2ffaafe7dfd1e8f29841e947995125
Agustin Radocaj <jaguscaj@gmail.com>
2024-07-17 11:09:27 -0300

    Primera parte

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../types';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient ) { }
+
+  get<t>(url: string, options: Options): Observable<t> {
+    return this.httpClient.get<t>(url, options) as Observable<t>
+  }
+}
