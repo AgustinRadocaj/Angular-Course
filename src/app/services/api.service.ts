35f3f4225f584d1964b73f0671e7027d70c3a37c
Agustin Radocaj <jaguscaj@gmail.com>
2024-07-23 12:28:32 -0300

    create, add & delete functions

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Options } from '../../types';
+import { Options, Product } from '../../types';
 
 
 @Injectable({
@@ -14,4 +14,17 @@ export class ApiService {
   get<t>(url: string, options: Options): Observable<t> {
     return this.httpClient.get<t>(url, options) as Observable<t>
   }
+
+  post<t>(url: string, body: Product, options: Options): Observable<t> {
+    return this.httpClient.post<t>(url, body, options) as Observable<t>
+  }
+
+  put<t>(url: string, body: Product, options: Options): Observable<t> {
+    return this.httpClient.put<t>(url, body, options) as Observable<t>
+  }
+
+  delete<t>(url: string, options: Options): Observable<t> {
+    return this.httpClient.delete<t>(url, options) as Observable<t>
+  }
 }
+
